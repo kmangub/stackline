6ac9fe6d8c0d4818a30b588d129553505f1d1d08
Alan Karlo Mangubat <karlomangubat@Alans-MacBook-Air.local>
2024-06-21 12:55:07 -0700

    Modified app.css app.tsx and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -12,15 +11,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </Provider>
   </React.StrictMode>
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 90047f7 (Changing template from JavaScript to TypeScript)
